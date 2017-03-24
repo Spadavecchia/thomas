22956aaaacde81ae9016e246d908ee57f943ded4
Antonio Abella <antonioabella@designiostecnologicos.com>
2017-03-24 08:29:49 -0400

    Initial commit

---
+++ lib/thomas.ex
@@ -0,0 +1,18 @@
+defmodule Thomas do
+  @moduledoc """
+  Documentation for Thomas.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Thomas.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/thomas_test.exs
@@ -0,0 +1,8 @@
+defmodule ThomasTest do
+  use ExUnit.Case
+  doctest Thomas
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
